b57007434546cce9e24d1df358ccad0c76b4f1bb
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-04-09 15:40:35 +0200

    method added to 'BankBranch' class

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ return bankBranchInstance;
 
 }
 // 4. Add methods to the `BankBranch` class for managing branch-related information. For example, you can add a method like `getBranchInfo` to retrieve branch details.
+getBranchInfo() {
+    return this.branchInfo;
+}
+}
 
 // 5. In the usage section:
 //    - Create instances of the `BankBranch` class, such as `branchA` and `branchB`, with different branch information.
