824fb36447dba6b69e63a1956469413fc5c52054
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-04-09 15:29:39 +0200

    created a constructor and a new instance

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ let bankBranchInstance;
 
 // 2. Define a class called `BankBranch` for managing branch information.
 class BankBranch {
-    constructor(branchInfo) {
         
 // 3. In the `BankBranch` class:
 //    - Create a constructor that takes `branchInfo` as a parameter.
 //    - Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
 //    - If `bankBranchInstance` is null, create a new instance with the provided `branchInfo` and assign it to `bankBranchInstance`.
 //    - Return the `bankBranchInstance` whether it's newly created or existing.
+constructor(branchInfo) {
+if (!bankBranchInstance) {
+this.branchInfo = branchInfo;
+bankBranchInstance = this;
+}
+return bankBranchInstance;
+}
 
+}
 // 4. Add methods to the `BankBranch` class for managing branch-related information. For example, you can add a method like `getBranchInfo` to retrieve branch details.
 
 // 5. In the usage section:
