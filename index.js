cad3d4143e44588eb970bcbe0db1a27019418038
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-04-09 15:20:31 +0200

    defined BankBranch class

--- index.js
+++ index.js
@@ -4,7 +4,9 @@
 let bankBranchInstance;
 
 // 2. Define a class called `BankBranch` for managing branch information.
-
+class BankBranch {
+    constructor(branchInfo) {
+        
 // 3. In the `BankBranch` class:
 //    - Create a constructor that takes `branchInfo` as a parameter.
 //    - Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
